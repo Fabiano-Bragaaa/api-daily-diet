0929229a98b5e9f8c4dc3dbfd371e56d1e6d8c64
Fabiano Braga <fabianobragadev@gmail.com>
2025-06-08 21:45:49 -0300

    feat: meals tests

--- src/routes/__tests__/users.test.ts
+++ src/routes/__tests__/users.test.ts
@@ -1,7 +1,14 @@
-import { afterAll, beforeAll, expect, test, describe } from "vitest";
+import {
+  afterAll,
+  beforeAll,
+  expect,
+  test,
+  describe,
+  beforeEach,
+} from "vitest";
 import request from "supertest";
 import { app } from "../../app";
-import { randomUUID } from "node:crypto";
+import { execSync } from "node:child_process";
 
 describe("users route", () => {
   beforeAll(async () => {
@@ -12,12 +19,31 @@ describe("users route", () => {
     await app.close();
   });
 
+  beforeEach(() => {
+    execSync("npm run knex migrate:rollback --all");
+    execSync("npm run knex migrate:latest");
+  });
+
   test("user can create account", async () => {
     const resonse = await request(app.server).post("/users/signUp").send({
-      email: "fabianobragaaaaa@gmail.com",
+      email: "fabianobraga@gmail.com",
       password: "Abc123",
     });
 
     expect(resonse.statusCode).toEqual(201);
+  });
+
+  test("user can login with your account", async () => {
+    await request(app.server).post("/users/signUp").send({
+      email: "fabianobraga@gmail.com",
+      password: "Abc123",
+    });
+
+    const signInUser = await request(app.server).post("/users/login").send({
+      email: "fabianobraga@gmail.com",
+      password: "Abc123",
+    });
+
+    expect(signInUser.statusCode).toEqual(201);
   });
 });
